7be51247b7b219e940fa945164abe6f0e69ad6cf
alan_flores <alan.flores@sanmina.com>
2016-09-13 17:31:44 -0500

    first commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,41 @@
+var app = angular.module('nodeTodo', []);
+
+app.controller('mainController', function($scope, $http) {
+
+    $scope.formData = {};
+    $scope.todoData = {};
+
+    // Get all todos
+    $http.get('/api/v1/todos')
+    .then(function(response) {
+        $scope.todoData = response.data;
+	console.log(response.data);
+    });
+    
+   $scope.createTodo = function(todoID) {
+    $http.post('/api/v1/todos', $scope.formData)
+        .success(function(data) {
+            $scope.formData = {};
+            $scope.todoData = data;
+            console.log(data);
+        })
+        .error(function(error) {
+            console.log('Error: ' + error);
+        });
+    };
+
+
+
+
+    $scope.deleteTodo = function(todoID) {
+    $http.delete('/api/v1/todos/' + todoID)
+        .success(function(data) {
+            $scope.todoData = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+    };
+
+});
